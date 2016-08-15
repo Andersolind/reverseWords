7756eb987fd2ad31f6afc4ccb0df3d191a7c0c56
Andersolind <andersolind@gmail.com>
2016-08-14 21:22:56 -0400

    Good place to learn
    ..

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+
+namespace Canada
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string questionOne = "I would really like to work at your company";
+            var c = new Common();
+            var getReversedString = c.ReverseString(questionOne);
+            //Question 2
+            var getValue = c.GetNumberOfPrime(45);
+            //Question 3 
+            var numberOne = "sindy";
+            var numberTwo = "Anderson";
+            //Question 3
+            var getResult = c.OrderByNSq(numberOne.ToCharArray(), numberTwo.ToCharArray());
+            var getResult2 = c.OrderByN(numberOne.ToCharArray(), numberTwo.ToCharArray());
+        }
+    }
+
+    internal interface ICommon
+    {
+        string ReverseString(string input);
+        int GetNumberOfPrime(int start);
+        char[] OrderByNSq(char[] a, char[] b);
+
+        char[] OrderByN(char[] a, char[] b);
+    }
+
+    public class Common : ICommon
+    {
+        public string ReverseString(string input)
+        {
+            //Convert the sting into a character set
+            var inputChars = input.ToCharArray();
+            //Lets reverse it
+            Array.Reverse(inputChars);
+            //Return your value
+            return new string(inputChars);
+        }
+
+        public int GetNumberOfPrime(int start)
+        {
+            var isPrime = 0;
+            for (var j = 2; j <= start; j++)
+            {
+                for (var k = 2; k <= start; k++)
+                {
+                    if (j == k)
+                    {
+                        isPrime++;
+                        break;
+                    }
+                    if (j%k == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            return isPrime;
+        }
+
+        public char[] OrderByNSq(char[] a, char[] b)
+        {
+            var ret = "";
+
+            foreach (var t in a)
+                if (b.Any(t1 => t == t1))
+                {
+                    if (ret.IndexOf(t) == -1)
+                        ret += t;
+                }
+
+            return ret.ToCharArray();
+        }
+
+        public char[] OrderByN(char[] a, char[] b)
+        {
+            var ret = "";
+            var flags = new int[256];
+
+            foreach (var t in b)
+                flags[t] = 1;
+
+            foreach (var t in a)
+                if (flags[t] == 1)
+                {
+                    ret += t;
+                    flags[t] = 0;
+                }
+            return ret.ToCharArray();
+        }
+    }
+}
---
+++ SqlQuestions.sql
@@ -0,0 +1,62 @@
+CREATE FUNCTION [dbo].[fn_ReverseWords] (
+@ip VARCHAR(MAX)
+)
+RETURNS VARCHAR(MAX)
+BEGIN
+DECLARE @op VARCHAR(MAX)
+SET @op = ''
+DECLARE @Lenght INT
+WHILE LEN(@ip) > 0
+BEGIN
+IF CHARINDEX(' ', @ip) > 0
+BEGIN
+SET @op = SUBSTRING(@ip,0,CHARINDEX(' ', @ip)) + ' ' + @op
+SET @ip = LTRIM(RTRIM(SUBSTRING(@ip,CHARINDEX(' ', @ip) + 1,LEN(@ip))))
+END
+ELSE
+BEGIN
+SET @op = @ip + ' ' + @op
+SET @ip = ''
+END
+END
+RETURN @op
+END
+
+Go
+---
+create PROCEDURE dbo.FindPrimeNumbers
+(
+	@PrimeNumbers INTEGER
+)
+AS
+BEGIN
+
+DECLARE @ints TABLE (
+    i    int NOT NULL,
+    PRIMARY KEY CLUSTERED (i)
+);
+
+--- Populate work table with a series of integers..
+WITH cte (i)
+AS (    -- .. starting at 2
+    SELECT 2 AS i UNION ALL
+    SELECT i+1 FROM cte WHERE i<@PrimeNumbers)
+
+INSERT INTO @ints (i)
+SELECT i FROM cte
+--- ... and we're already excluding some "obvious" primes,
+--- by removing values that are divisible with 2, 3, 5, 7
+--- and 11. This keeps the table size down and improves
+--- performance.
+WHERE (i=2 OR i%2!=0) AND
+    (i=3 OR i%3!=0) AND
+    (i=5 OR i%5!=0) AND
+    (i=7 OR i%7!=0) AND
+    (i=11 OR i%11!=0)
+OPTION (MAXRECURSION 0)
+
+DECLARE @p int, @i_count int
+SELECT @p=1, @i_count=MAX(i) FROM @ints
+end
+
+go
